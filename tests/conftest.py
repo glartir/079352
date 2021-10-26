4e188724bbb4af1ffe4ab9ac29a1de5104d76984
glartir <sidorenko.slava@gmail.com>
2021-10-26 16:04:43 +0300

    health check workaround v5

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def browser():
     options.add_argument("--start-maximized")  # open Browser in maximized mode
     options.add_argument("--no-sandbox")  # bypass OS security model
     options.add_argument('--disable-dev-shm-usage')
-    browser = webdriver.Chrome(options=options, executable_path="/home/viacheslav_sidorenko/PycharmProjects/079352/chromedriver")
+    browser = webdriver.Chrome(options=options,)
     # browser.maximize_window()
     yield browser
     browser.quit()
