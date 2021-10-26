ccc1d45ad4bd5638c4769629a01e63286c97f5af
glartir <sidorenko.slava@gmail.com>
2021-10-26 14:48:01 +0300

    health check workaround v2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def browser():
-
-    browser = webdriver.Chrome(executable_path="/home/viacheslav_sidorenko/PycharmProjects/079352/chromedriver")
-    browser.maximize_window()
+    options = Options()
+    options.add_argument("--start-maximized")  # open Browser in maximized mode
+    options.add_argument("--no-sandbox")  # bypass OS security model
+    browser = webdriver.Chrome(options=options, executable_path="/home/viacheslav_sidorenko/PycharmProjects/079352/chromedriver")
+    # browser.maximize_window()
     yield browser
     browser.quit()
