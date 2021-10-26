cd12cf6c5e2a93a2ccb415ae5abe0b7f8c19d73f
glartir <sidorenko.slava@gmail.com>
2021-10-26 14:41:02 +0300

    health check workaround

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 @pytest.fixture
 def browser():
 
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(executable_path="/home/viacheslav_sidorenko/PycharmProjects/079352/chromedriver")
     browser.maximize_window()
     yield browser
     browser.quit()
